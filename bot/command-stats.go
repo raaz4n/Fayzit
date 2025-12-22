81d928aee17e9742a792ee0599e3997aee51a1e6
raaz4n <raazandoski@gmail.com>
2025-12-21 23:49:13 -0600

    added lvl emotes, will add challenger emote later

--- bot/command-stats.go
+++ bot/command-stats.go
@@ -13,14 +13,16 @@ import (
 )
 
 const pURL string = "https://open.faceit.com/data/v4/players"
+const lURL string = "https://open.faceit.com/data/v4/leaderboards"
 
 type FaceitData struct {
 	Avatar string `json:"avatar"`
 	Games  struct {
 		Cs2 struct {
-			Faceit_Elo  int64  `json:"faceit_elo"`
-			Region      string `json:"region"`
-			Skill_level int64  `json:"skill_level"`
+			Faceit_Elo        int64  `json:"faceit_elo"`
+			Region            string `json:"region"`
+			Skill_level       int64  `json:"skill_level"`
+			Skill_level_label string `json:"skill_level_label"`
 		} `json:"cs2"`
 	} `json:"games"`
 	Nickname string `json:"nickname"`
@@ -29,6 +31,7 @@ type FaceitData struct {
 
 func getCurrentStats(message string, searchType string) *discordgo.MessageSend {
 	var faceitURL string
+	//var leaderboardURL string
 
 	switch searchType {
 	case "faceit-username":
@@ -83,6 +86,30 @@ func getCurrentStats(message string, searchType string) *discordgo.MessageSend {
 	uppercountry := strings.ToUpper(usercountry)
 
 	var regionstring string
+	var lvlstring string
+
+	switch faceitlvl {
+	case "1":
+		lvlstring = "<:1_:1452534536935178371>"
+	case "2":
+		lvlstring = "<:2_:1452534559748001924>"
+	case "3":
+		lvlstring = "<:3_:1452534579779997848>"
+	case "4":
+		lvlstring = "<:4_:1452534669869322472>"
+	case "5":
+		lvlstring = "<:5_:1452534684268626130>"
+	case "6":
+		lvlstring = "<:6_:1452534697698787328>"
+	case "7":
+		lvlstring = "<:7_:1452534713939132600>"
+	case "8":
+		lvlstring = "<:8_:1452534729583628361>"
+	case "9":
+		lvlstring = "<:9_:1452534745366921278>"
+	case "10":
+		lvlstring = "<:10:1452534760843902987>"
+	}
 
 	switch region {
 	case "EU":
@@ -106,7 +133,7 @@ func getCurrentStats(message string, searchType string) *discordgo.MessageSend {
 				},
 				{
 					Name:   "Level",
-					Value:  faceitlvl,
+					Value:  "  " + lvlstring,
 					Inline: true,
 				},
 				{
