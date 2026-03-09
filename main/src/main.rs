b2f38780ab3953a371c5d78304200ccd8f971a54
raaz4n <raazandoski@gmail.com>
2026-03-09 17:48:39 -0500

    bot startup for main

--- main/src/main.rs
+++ main/src/main.rs
@@ -2,7 +2,6 @@ use dotenv::dotenv;
 use std::env;
 
 use serenity::async_trait;
-use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
@@ -10,25 +9,15 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // handler for 'message', to be called when a new message is received
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "Hello" {
-            // if the message fails due to anything
-            if let Err(why) = msg.channel_id.say(&ctx.http, "World!").await {
-                println!("Error sending msg: {why:?}");
-            }
-        }
-    }
-
-    // once bot is connected, give the status update
+    // placeholder function for checking bot status
     async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+        println!("{} is connected!", ready.user.name)
     }
 }
 
 #[tokio::main]
 async fn main() {
-    // load .env, ignor errors
+    // load .env, ignore errors
     dotenv().ok();
     // client config with .env bot token
     let token = env::var("TOKEN").expect("Need a token for the Discord bot");
@@ -38,11 +27,10 @@ async fn main() {
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-    // new instance of the Client which will tie in with the discord bot token
-    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    // client setup
+    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Error creating client");
 
-    // listen to events
     if let Err(why) = client.start().await {
-        println!("Client error: {why:?}");
+        println!("Client err: {why:?}");
     }
 }
