cfdae8adc908f58f6749ac702f0ad84ec43a7662
raaz4n <raazandoski@gmail.com>
2026-03-09 18:35:06 -0500

    implementing search function

---
+++ main/src/commands/search.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+
+const S_URL: &str = "https://open.faceit.com/data/v4/search/players?";
+
+// deserialization of the JSON
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SearchData {
+    items: Vec<SearchItems>, // going through a vector of nicknames once search is called to find the right user
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SearchItems {
+    nickname: String,
+}
+
+pub async fn search_user(_message: &str) -> String {
+    let test = String::new();
+    test
+}
